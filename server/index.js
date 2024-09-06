10ba64343beee7a0fdef96280109abae6660c7c0
Anurag-Band <anuragband10@gmail.com>
2024-09-06 19:39:36 +0530

    feat: error handlers added

--- server/index.js
+++ server/index.js
@@ -31,5 +31,18 @@ app.use("/api/campaigns", campaignRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  process.exit(1);
+});
+
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
 
 
